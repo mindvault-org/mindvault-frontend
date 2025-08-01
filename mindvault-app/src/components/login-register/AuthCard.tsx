2652bbe1f92392c276c965ffb23d314f543dc3a3
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-08-01 15:21:28 +0200

    feat: call API routes to complete login and registration fonctionnalities

--- mindvault-app/src/components/login-register/AuthCard.tsx
+++ mindvault-app/src/components/login-register/AuthCard.tsx
@@ -2,6 +2,7 @@
 
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
+import { useRouter } from "next/navigation";
 
 export default function AuthCard({
   isLogin,
@@ -10,13 +11,63 @@ export default function AuthCard({
   isLogin: boolean;
   setIsLogin: (value: boolean) => void;
 }) {
+  const handleRegister = async (formData: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+  }) => {
+    try {
+      const response = await fetch("http://localhost:8080/api/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) throw new Error("Error during registration");
+
+      const data = await response.json();
+      console.log("Registration successful:", data.message);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const router = useRouter();
+  const handleLogin = async (credentials: {
+    email: string;
+    password: string;
+  }) => {
+    try {
+      const response = await fetch("http://localhost:8080/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+      });
+
+      if (!response.ok) throw new Error("Login failed");
+
+      const data = await response.json();
+      const token = data.token;
+
+      localStorage.setItem("token", token);
+
+      router.push("/dashboard");
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div className="w-full max-w-md rounded-2xl backdrop-blur-xl shadow-lg border border-gray-200 bg-white/70 p-8">
       <h2 className="text-2xl font-bold text-center mb-6 text-gray-800">
-        {isLogin ? "Se connecter" : "Créer un compte"}
+        {isLogin ? "Login" : "Register"}
       </h2>
 
-      {isLogin ? <LoginForm /> : <RegisterForm />}
+      {isLogin ? (
+        <LoginForm onSubmit={handleLogin} />
+      ) : (
+        <RegisterForm onSubmit={handleRegister} />
+      )}
 
       <div className="text-center mt-6 text-sm text-gray-600">
         {isLogin ? (
