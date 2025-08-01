c157a92daaeb642074b229bbce822e54771bceec
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-08-01 14:57:29 +0200

    feat: create simple login form

---
+++ mindvault-app/src/components/login-register/LoginForm.tsx
@@ -0,0 +1,22 @@
+export default function LoginForm() {
+  return (
+    <form className="space-y-4">
+      <input
+        type="email"
+        placeholder="Adresse e-mail"
+        className="w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+      />
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        className="w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+      />
+      <button
+        type="submit"
+        className="w-full bg-gradient-to-tr from-[#D56434] to-[#6D66E7] text-white py-2 rounded-lg font-medium hover:opacity-90 transition-all"
+      >
+        Login
+      </button>
+    </form>
+  );
+}
